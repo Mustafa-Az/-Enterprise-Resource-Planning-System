9d1eaee20b737ef29d668860524e272287011e6e
Mustafa AZ <mustafa.22.abdulaziz@gmail.com>
2024-07-17 10:29:45 +0200

    Setup Maven Project

---
+++ src/test/java/dev/mustafa/erp_system/ErpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mustafa.erp_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
